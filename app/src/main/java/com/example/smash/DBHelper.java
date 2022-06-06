953003e930004df25dfdea82b75cf5ff0f6ac1ba
CherryBlossom99 <viptaeyeon@gmail.com>
2022-06-07 03:01:01 +0900

    로그인 제작중 1 - PTY

--- app/src/main/java/com/example/smash/DBHelper.java
+++ app/src/main/java/com/example/smash/DBHelper.java
@@ -1,59 +1,56 @@
 package com.example.smash;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import androidx.annotation.Nullable;
 
-public class DBHelper extends SQLiteOpenHelper{
-    static  final String DATABASE_NAME = "test.db";
+public class DBHelper extends SQLiteOpenHelper {
 
-    public DBHelper(Context context, int version){
-        super(context, DATABASE_NAME, null, version);
+    public static final String DBNAME = "Login.db";
+
+    public DBHelper(@Nullable Context context) {
+        super(context, "Login.db", null, 1);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE Person(name TEXT, Age INT, ADDR TEXT)");
+    public void onCreate(SQLiteDatabase MyDB) {
+        MyDB.execSQL("create Table users(username TEXT primary key, password TEXT)");
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS Person");
-        onCreate(db);
+    public void onUpgrade(SQLiteDatabase MyDB, int oldVersion, int newVersion) {
+        MyDB.execSQL("drop Table if exists users");
     }
 
-    public void insert(String School, String Name, String PassWord){
-        SQLiteDatabase db = getWritableDatabase();
-        db.execSQL("INSERT INTO Person VALUES('"+ School + "', " + Name + ", '"+ PassWord + "')");
-        db.close();
+    public Boolean insertData(String username, String password){
+        SQLiteDatabase MyDB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("username", username);
+        contentValues.put("password", password);
+        long result = MyDB.insert("users", null, contentValues);
+        if (result==-1) return false;
+        else
+            return true;
     }
 
-    public void Update(String School, String Name, String PassWord){
-        SQLiteDatabase db = getWritableDatabase();
-        db.execSQL("UPDATE Person SET School = " + School + ", PassWord = '" + PassWord + "'" + " WHERE NAME = '" + Name + "'");
+    public Boolean checkusername(String username) {
+        SQLiteDatabase MyDB = this.getWritableDatabase();
+        Cursor cursor = MyDB.rawQuery("Select * from users where username = ?", new String[] {username});
+        if (cursor.getCount()>0)
+            return true;
+        else
+            return false;
     }
 
-    public void Delete(String Name){
-        SQLiteDatabase db = getWritableDatabase();
-        db.execSQL("DELETE Person WHERE NAME = '" + Name + "'");
-        db.close();
+    public Boolean checkusernamepassword(String school, String username, String password) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("Select * from users where school = ? and username = ? and password = ?", new String[] {school,username,password});
+        if (cursor.getCount()>0)
+            return true;
+        else
+            return false;
     }
-
-    public String getResult(){
-        SQLiteDatabase db = getReadableDatabase();
-        String result = "";
-
-        Cursor cursor = db.rawQuery("SELECT * FROM Person", null);
-        while(cursor.moveToNext()){
-            result += "학교 : " + cursor.getString(0)
-                    + ", 이름 : "
-                    + cursor.getString(1)
-                    + ", 비밀번호 : "
-                    + cursor.getString(2)
-                    +"\n";
-        }
-        return result;
-    }
-
 }
--- app/src/main/java/com/example/smash/LoginActivity.java
+++ app/src/main/java/com/example/smash/LoginActivity.java
@@ -1,27 +1,54 @@
 package com.example.smash;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button login;
+    Button btn_login;
+    EditText text_school, text_user, text_key;
+
+    // DBHelper
+    DBHelper DB;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         setTitle("Smash");
 
-        Button login = (Button) findViewById(R.id.btn_login);
-        login.setOnClickListener(new View.OnClickListener() {
+        btn_login = (Button) findViewById(R.id.btn_login);
+
+        text_school = (EditText) findViewById(R.id.text_school);
+        text_user = (EditText) findViewById(R.id.text_user);
+        text_key = (EditText) findViewById(R.id.text_key);
+
+        DB = new DBHelper(this);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent (getApplicationContext(), SetupActivity.class);
-                startActivity(intent);
+                String school = text_school.getText().toString();
+                String user = text_user.getText().toString();
+                String key = text_key.getText().toString();
+
+                if(school.equals("")||user.equals("")||key.equals(""))
+                    Toast.makeText(LoginActivity.this, "Fill up all fields", Toast.LENGTH_SHORT).show();
+                else{
+                    Boolean checkuserpass = DB.checkusernamepassword(school, user, key);
+                    if(checkuserpass){
+                        Toast.makeText(LoginActivity.this, "Sign in successful", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), SetupActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Invalid credentials", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
     }
