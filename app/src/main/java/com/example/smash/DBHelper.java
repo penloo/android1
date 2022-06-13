d67d55200a3286cfa2c56c5d195688bd8d8c43d6
Taeyeon Park <106387717+CherryBlossom99@users.noreply.github.com>
2022-06-14 04:57:33 +0900

    TQ 나 성공한거 같애 ㅠ

--- app/src/main/java/com/example/smash/DBHelper.java
+++ app/src/main/java/com/example/smash/DBHelper.java
@@ -6,22 +6,24 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import androidx.annotation.Nullable;
+
 public class DBHelper extends SQLiteOpenHelper {
 
     public static final String DBNAME = "Login.db";
 
-    public DBHelper(Context context) {
-        super(context, "groupDB", null, 1);
+    public DBHelper(@Nullable Context context) {
+        super(context, "Login.db", null, 1);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase MyDB) {
-        MyDB.execSQL("create Table users(school TEXT, user TEXT primary key, password TEXT)");
+    public void onCreate(SQLiteDatabase Login) {
+        Login.execSQL("create Table users(school TEXT, user TEXT primary key, password TEXT)");
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase MyDB, int oldVersion, int newVersion) {
-        MyDB.execSQL("drop Table if exists users");
+    public void onUpgrade(SQLiteDatabase Login, int oldVersion, int newVersion) {
+        Login.execSQL("drop Table if exists users");
     }
 
     public Boolean insertData(String school, String user, String password) {
