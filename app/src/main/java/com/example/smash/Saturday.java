62912bc5604254df608ba2f83be3cb143f112919
penloo <44522204+penloo@users.noreply.github.com>
2022-06-19 13:49:01 +0900

    3-4

---
+++ app/src/main/java/com/example/smash/Saturday.java
@@ -0,0 +1,13 @@
+package com.example.smash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
+
+public class Saturday extends AppCompatActivity {
+    private final Calendar calendar = Calendar.getInstance();
+
+    public Saturday(){
+
+    }
+}
